a60345f7ad930df1da66d93fdb7d381efeb58cce
Biography J <tjglobal@tjglobal.co.kr>
2023-01-02 01:14:21 +0900

    update

--- Basic_Practice_BJ_1003.py
+++ Basic_Practice_BJ_1003.py
@@ -1,3 +1,17 @@
 # 백준 1003 # 
 # 피보나치 수열 #
 
+
+def fibo(N):
+    a, b = 0, 1
+    if N == 0:
+        a, b = 0, 0
+    for i in range(N-1):
+        a, b = b, a+b
+    
+    return a, b
+
+d = fibo(10)
+print(d)
+
+ 
