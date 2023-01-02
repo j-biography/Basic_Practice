1dae02e21de013e72a297350f804ac0240aab44d
Biography J <tjglobal@tjglobal.co.kr>
2023-01-02 18:44:40 +0900

    update

--- Basic_Practice_BJ_1003.py
+++ Basic_Practice_BJ_1003.py
@@ -2,16 +2,32 @@
 # 피보나치 수열 #
 
 
-def fibo(N):
+# def fibo(N):
+#     a, b = 0, 1
+#     if N == 0:
+#         a, b = 0, 0
+#     for i in range(N-1):
+#         a, b = b, a+b
+    
+#     return a, b
+
+# d = fibo(10)
+# print(d)
+
+
+
+T = int(input())
+
+def fibonacci(N):
     a, b = 0, 1
     if N == 0:
-        a, b = 0, 0
-    for i in range(N-1):
-        a, b = b, a+b
-    
-    return a, b
+        a, b = 1, 0
+    elif N == 1:
+        a, b = 0, 1
+    else:
+        for i in range(N-1):
+            a, b = b, a+b
+    print(a, b)
 
-d = fibo(10)
-print(d)
-
- 
+for i in range(T):
+    fibonacci(int(input()))
