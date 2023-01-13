0621b0484424007646ca160fabbc8d14444d4e81
Biography J <tjglobal@tjglobal.co.kr>
2023-01-13 12:35:41 +0900

    update

---
+++ Basic_Questions_TestField_1.py
@@ -0,0 +1,80 @@
+
+# def plus(c, d):
+#     print(c+d)
+
+# a, b = map(int, input().split())
+# plus(a, b)
+
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+        
+#     print(i)
+# print("Done")
+
+
+
+# A, B = map(int, input().split())
+# print(A/B)
+
+# D = [0] * 10
+
+# print(D)
+
+
+# A, B = map(int, input().split())
+
+# print(A+B, A-B, A*B, A/B, A%B)
+# print(A-B)
+# print(A*B)
+# print(A/B
+# print(A%B))
+
+
+# A,B=map(int, input().split())
+
+# print(A+B,A-B,A*B,A//B,A%B,sep='\n')
+
+
+
+# A, B, C = map(int, input().split())
+
+# class Formula:
+#     def f1(A,B,C):
+#         print((A+B)%C)
+     
+#     def f2(A,B,C):
+#         print(((A%C)+(B%C))%C)
+        
+#     def f3(A,B,C):
+#         print((A*B)%C)
+        
+#     def f4(A,B,C):
+#         print(((A%C)*(B%C))%C)
+        
+# Formula.f1(A,B,C)
+# Formula.f2(A,B,C)
+# Formula.f3(A,B,C)
+# Formula.f4(A,B,C)
+
+
+
+# A = input()
+# B = input()
+
+# A_int = int(A)
+# B_int = int(B)
+
+# print(A_int * int(B[-1]))
+# print(A_int * int(B[-2]))
+# print(A_int * int(B[-3]))
+# print(A_int * B_int)
+
+
+
+# print("\    /\ ")
+# print(" )  ( ')")
+# print("(  /  )")
+# print(" \(__)|")
+
