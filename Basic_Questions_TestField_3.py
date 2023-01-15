9353c9d84bcff287608af50d52eddb053a64f2f6
Biography J <tjglobal@tjglobal.co.kr>
2023-01-16 03:01:03 +0900

    update

---
+++ Basic_Questions_TestField_3.py
@@ -0,0 +1,22 @@
+
+
+# 2739
+
+
+N = int(input())
+
+for i in range(1,10):
+    print(N,'*',i,'=',N*i)
+    
+
+# 10950
+
+T = int(input())
+
+for i in range(T):
+    A, B = map(int, input().split())
+    
+    print(A+B)
+    
+    
+# 8393
