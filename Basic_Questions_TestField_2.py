69d11c19c93c23bb38017fd181ea5e33f854aae3
Biography J <tjglobal@tjglobal.co.kr>
2023-01-16 03:01:06 +0900

    update

--- Basic_Questions_TestField_2.py
+++ Basic_Questions_TestField_2.py
@@ -1,3 +1,5 @@
+
+
 # print("|\_/|")
 # print("|q p|   /}")
 # print('( 0 )"""\ ')
@@ -13,6 +15,7 @@
 # print('      |')
 # print('      |')
 
+'''
 
 # A, B = map(int, input().split())
 
@@ -41,6 +44,11 @@
 #     print('F')
 
 
+'''
+
+# 윤년 #
+
+'''
 Year = int(input())
 
 if Year % 4 == 0:
@@ -51,3 +59,173 @@ if Year % 4 == 0:
 else:
     print('0')
     
+'''
+ # 사분면 #
+ 
+ 
+'''
+
+Quadrant 1 : X = plus, Y = plus
+Quadrant 2 : X = minus, Y = plus
+Quadrant 3 : X = minus, Y = minus
+Quadrant 4 : X = plus, Y = minus 
+
+
+X = int(input())
+Y = int(input())
+
+def Quadrant(X, Y):
+    if X >= 0 and Y >= 0:
+        print(1)
+        
+    elif X < 0 and Y >= 0:
+        print(2)
+        
+    elif X < 0 and Y < 0:
+        print(3)
+
+    elif X >= 0 and Y >= 0:
+        print(4)
+   
+   
+   
+X = int(input())
+Y = int(input())
+
+def Quadrant(X, Y):
+    if X >= 0:
+        if Y >= 0:
+            print(1)
+        
+        elif Y < 0:
+            print(4) 
+    
+    elif X < 0:
+        if Y >= 0:
+            print(2)
+            
+        elif Y < 0:
+            print(3)
+            
+Quadrant(X, Y)
+
+'''
+
+# 2884 알람
+
+'''
+
+X, Y = map(int, input().split())
+
+def Hour(X, Y):
+    if 1 <= X <= 23 and Y >= 45:
+        X = X
+        if 45 <= Y <= 59:
+            Y -= 45
+        elif Y <= 44:
+            Y = Y + 15
+            
+    elif 1 <= X <= 23 and Y <= 44:
+        X -= 1
+        if 45 <= Y <= 59:
+            Y -= 45
+        elif Y <= 44:
+            Y = Y + 15
+        
+    elif X == 0 and Y >= 45:
+        X = 0
+        if 45 <= Y <= 59:
+            Y -= 45
+        elif Y <= 44:
+            Y = Y + 15  
+            
+    else:
+        X = 23
+        if 45 <= Y <= 59:
+            Y -= 45
+        elif Y <= 44:
+            Y = Y + 15  
+            
+    print(X, Y)
+    
+Hour(X,Y)
+
+'''
+
+'''
+X, Y = map(int, input().split())
+
+def Hour(X, Y):
+    if 1 <= X <= 23:
+        if 45 <= Y:
+            X = X
+            Y -= 45
+        elif Y <= 44:
+            X -= 1
+            Y += 15
+        
+    elif X == 0:
+        if 45 <= Y <= 59:
+            X = X
+            Y -= 45
+        elif Y <= 44:
+            X = 23
+            Y += 15  
+            
+    print(X, Y)
+    
+Hour(X,Y)
+'''
+
+# 2525 오븐
+
+'''
+
+X, Y = map(int, input().split())
+Time = int(input())
+
+def Oven(X, Y):
+    
+    if Y + Time >= 60:
+       
+        if (X + (Y + Time) // 60) < 24:
+            X += (Y + Time) // 60
+            Y = (Y + Time) % 60
+        else:
+            X = (X + (Y + Time)  // 60) - 24
+            Y = (Y + Time) % 60
+           
+    else:
+        X = X
+        Y += Time
+            
+    print(X, Y)
+    
+Oven(X, Y)
+
+'''
+
+# 2480 주사위
+
+X, Y, Z = map(int, input().split())
+
+def dice(X, Y, Z):
+    if X == Y and X == Z:
+        print((X * 1000) + 10000)
+    
+    elif X == Y or X == Z:
+        print((X * 100) + 1000)
+    
+    elif Y == Z:
+        print((Y * 100) + 1000)
+        
+    else:
+        if X > Y and X > Z:
+            print(X*100)
+        elif Y > X and Y > Z:
+            print(Y * 100)
+        else:
+            print(Z * 100)
+        
+dice(X, Y, Z)
+
