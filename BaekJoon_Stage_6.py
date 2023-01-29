77838c40ad0fcf5e54123abc774db56fdb7b0a9a
j-biography <juhjj147@naver.com>
2023-01-29 20:44:54 +0900

    update

--- BaekJoon_Stage_6.py
+++ BaekJoon_Stage_6.py
@@ -47,4 +47,141 @@ for i in range(len(x)):
 print(sum(a))    
 '''
 
-#
+# 10809
+
+# index 기능으로 위치를 찾아낼 것
+
+'''
+s = input()
+
+s_list = list(s)
+
+alphabet = ['a','b','c','d','e',
+            'f','g','h','i','j',
+            'k','l','m','n','o',
+            'p','q','r','s','t',
+            'u','v','w','x','y','z']
+
+for i in alphabet:
+    if i in s_list:
+        print(s_list.index(i), end=' ')
+    else:
+        print('-1', end=' ')
+        '''
+    
+# find 함수는 찾는 값이 없을 경우 -1을 출력하는 것을 활용.
+    
+'''
+s = input()
+
+for i in range(97, 123):
+    print(s.find(chr(i)), end=' ')
+    '''
+    
+# 2675
+
+'''
+T = int(input())
+
+for test_case in range(T):
+    
+    R, S = input().split()
+    
+    for repeat in range(len(S)):
+        print(S[repeat]*int(R), end='')
+    print()
+    '''
+    
+# 1157
+
+# 1차 코딩 실패 ver.
+
+'''
+voca = input().upper()
+voca_list = []
+count_value = 0
+
+for i in range(len(voca)):
+    voca_list.append(ord(voca[i]))
+
+voca_list2 = list(set(voca_list))
+
+for j in range(len(voca_list2)):
+    value = voca_list.count(voca_list2[j])
+    if value >= count_value:
+        count_value = value 
+    
+print(count_value)
+'''
+
+# 완성하였으나 복잡. 코딩에 과도한 시간 소요되었으며, 런타임도 1000ms 이상. 복기해볼 것.
+
+'''
+N = input().upper()
+
+a = []
+b = []
+count = 0
+max = 0
+
+for i in range(len(N)):
+    a.append(ord(N[i]))
+    if ord(N[i]) not in b:
+        b.append(ord(N[i]))
+        
+for j in range(len(b)):
+    if a.count(b[j]) > count:
+        count = a.count(b[j])
+        max = b[j]
+    elif a.count(b[j]) == count:
+        max = 63
+    
+print(chr(max))
+'''
+
+# 축약하여 2차 코딩 진행하였으나 런타임을 비약적으로 줄이지 못함.
+
+'''
+N = list(input().upper())
+
+max = 0
+check = 0
+
+for i in range(65, 91):
+    if N.count(chr(i)) > check:
+        check = N.count(chr(i))
+        max = chr(i)
+    elif N.count(chr(i)) == check:
+        max = '?'
+        
+print(max)
+'''
+
+# 3차 코딩. 획기적인 런타임 시간 단축. max값이 있는 위치(index)를 통해 최다 사용 알파벳을 역산출.
+
+'''
+N = input().upper()
+
+a = []
+
+for i in range(65, 91):
+    a.append(N.count(chr(i)))
+
+if a.count(max(a)) >= 2:
+    print('?')
+    
+else:
+    b = a.index(max(a))
+    print(chr(65+b))
+    '''
+
+# 1152
+
+
+
+    
+        
+
+
+    
+    
