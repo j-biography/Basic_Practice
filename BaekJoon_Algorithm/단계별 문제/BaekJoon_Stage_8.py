3f8c111a82ec20a94f388703f8f1b2eb5c319384
j-biography <juhjj147@naver.com>
2023-02-10 22:31:40 +0900

    update

--- BaekJoon_Algorithm/단계별 문제/BaekJoon_Stage_8.py
+++ BaekJoon_Algorithm/단계별 문제/BaekJoon_Stage_8.py
@@ -35,3 +35,189 @@ for num in nums:
 print(count)
 '''
 
+# 2581
+
+# 시간초과 : 2028ms
+'''
+M = int(input()) # 첫째 줄에 주어지는 자연수, N보다 작거나 같다.
+N = int(input()) # 둘째 줄에 주어지는 자연수
+num = list(range(M,N+1))
+
+for i in range(M, N+1):
+    for j in range(N+1):
+        if i == 1 or j > 1 and i != j and i % j == 0:
+            num.remove(i)
+            break
+
+if len(num) >= 1:
+    print(sum(num))
+    print(min(num))
+else:
+    print('-1')
+    '''
+    
+# 시간초과 : 1056ms
+'''
+M = int(input())
+N = int(input())
+prime = []
+
+for i in range(M, N+1):
+    for j in range(2, N+1):
+        if i == 1 or (i != j and i % j == 0):
+            break
+        else:
+            if i % j != 0:
+                continue
+            else:
+                prime.append(i)
+                break
+                        
+if len(prime) < 1:
+    print('-1')
+else:
+    print(sum(prime))
+    print(min(prime))
+    '''
+
+# 수의 규칙(제곱근 이하의 수로 나눠지지 않는다면, 그 이상의 수로도 나뉘지 않는다.)
+# 116ms
+'''
+import math
+
+M = int(input())
+N = int(input())
+prime = []
+
+for i in range(M, N+1):
+    for j in range(2, int(math.sqrt(N)+2)):
+        if i == 1 or i < j or (i != j and i % j == 0):
+            break
+        elif i == j or int(math.sqrt(N)+1) == j:
+            prime.append(i)
+        else:
+            continue
+           
+if len(prime) < 1:
+    print('-1')
+else:
+    print(sum(prime))
+    print(min(prime))          
+'''
+
+# 소수를 찾아주는 함수를 정의해서 사용
+# 52ms
+'''
+import math
+
+def prime(n):
+    for i in range(2, int(math.sqrt(n))+2):
+        if n == 1 or n != i and n % i == 0:
+            return False
+    return True
+
+###############################
+
+M = int(input())
+N = int(input())
+number = range(M,N+1)
+list_prime = []
+
+for j in range(len(number)):
+    if prime(number[j]) == True:
+        list_prime.append(number[j])
+
+
+if len(list_prime) < 1:
+    print('-1')
+else:
+    print(sum(list_prime))
+    print(min(list_prime))
+    '''
+    
+# 11653  
+
+# 브루트 포스
+# 1312ms
+'''
+N = int(input())
+
+for i in range(2, N+1):
+    while N % i == 0:
+        N /= i
+        print(i)
+'''        
+        
+# 제곱근으로 범위를 축소시켜 브루트 포스
+# 44ms
+'''
+import math
+
+N = int(input())
+
+for i in range(2, int(math.sqrt(N))+2):
+    while N % i == 0:
+        N /= i
+        print(i)
+if N > 1:
+    print(int(N))
+    '''
+    
+# 1929
+
+# 문제 1978의 52ms 답안 그대로 사용 >>> 4056ms
+'''
+import math
+
+def prime(n):
+    for i in range(2, int(math.sqrt(n))+2):
+        if n == 1 or n != i and n % i == 0:
+            return False
+    return True
+
+M, N = map(int, input().split())
+
+number = range(M,N+1)
+list_prime = []
+
+for j in range(len(number)):
+    if prime(number[j]) == True:
+        print(number[j])
+        '''
+
+# 미리 2와 3의 배수 필터링 후 소수 판별 함수에 투입 >>> 3852ms(효과 미미)
+'''
+import math
+
+def prime(n):
+    for i in range(2, int(math.sqrt(n))+2):
+        if n == 1 or n != i and n % i == 0:
+            return False
+    return True
+
+M, N = map(int, input().split())
+
+number = []
+
+for j in range(M, N+1):
+    if j > 2 and j % 2 == 0:
+        continue
+    elif j > 3 and j % 3 == 0:
+        continue
+    else:
+        number.append(j)
+
+for k in range(len(number)):
+    if prime(number[k]) == True:
+        print(number[k])
+'''    
+        
+# '에라토스테네스의 채' 알고리즘 활용
+import math
+
+M, N = map(int, input().split())
+
+prime = list(range(M, N+1))
+
+for i in range(int(math.sqrt(N)+2)):
+    print(i)
