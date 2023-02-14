f6e58e649cc7297b891b78f3eaa5737a75ffda73
j-biography <juhjj147@naver.com>
2023-02-14 12:18:26 +0900

    update

--- Programmers/분수의 덧셈.py
+++ Programmers/분수의 덧셈.py
@@ -9,7 +9,41 @@
 # Constraints :
 # 0 <numer1, denom1, numer2, denom2 < 1,000
 
+
+# 정답률 93.3%
+'''
 num1, num2, dem1, dem2 = map(int, input().split())
 
 dem3 = max(dem1, dem2)
+
+while dem3 % dem1 != 0 or dem3 % dem2 != 0:
+    dem3 += 1
+    
+num1 = num1 * (dem3/dem1)
+num2 = num2 * (dem3/dem2)
+num3 = num1 + num2
+
+for i in range(2, dem3+1):
+    if num3 % i == 0 and dem3 % i == 0:
+        num3 = num3 / i
+        dem3 = dem3 / i
+        
+print([num3, dem3])
+'''
+
+# 정답률 100%
+'''
+def solution(numer1, denom1, numer2, denom2):
+    
+    denom3 = denom1 * denom2
+    
+    numer3 = numer1 * (denom3/denom1) + numer2 * (denom3/denom2)
+    
+    for i in range(denom3, 1, -1):
+        if denom3 % i == 0 and numer3 % i == 0:
+            denom3 /= i
+            numer3 /= i
+            
+    return [numer3, denom3]
+    '''
 
