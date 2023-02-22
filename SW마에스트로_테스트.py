ef35caae2d104e55be3343e5b5bfc72b8b333ef4
j-biography <juhjj147@naver.com>
2023-02-22 22:16:40 +0900

    update

---
+++ SW마에스트로_테스트.py
@@ -0,0 +1,23 @@
+
+
+import math
+
+def solution(nums):
+    
+    nums.sort()
+    max_sum = sum(nums[-1:-4:-1])
+    min_sum = sum(nums[0:3])
+    
+    prime = [i for i in range(min_sum, max_sum+1)]
+    
+    for i in range(len(prime)):
+        for j in range(2, int(math.sqrt(max_sum))+1):
+            if prime[i] != j and prime[i] % j == 0 and prime[i] in prime:
+                prime.remove(prime[i])
+            else:
+                continue
+    return prime
+
+nums = [1,2,7,6,4]
+
+print(solution(nums))
